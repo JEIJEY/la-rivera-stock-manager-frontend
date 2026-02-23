4dea10c2565330e94a3f19dddfb13592ab177608
Jeison Ferney Avila Quintero <jeijey.avi@gmail.com>
2026-02-22 21:19:01 -0500

    feat: configurar entorno base con Vite y logger Pino

---
+++ src/js/core/logger.js
@@ -0,0 +1,10 @@
+import pino from "pino";
+
+const logger = pino({
+  level: "debug",
+  browser: {
+    asObject: true
+  }
+});
+
+export default logger;
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "./styles/index.css";
+import logger from "./js/core/logger.js";
+
+logger.info("Frontend iniciado correctamente");
